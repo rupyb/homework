234374f169cc7b6ea9687fb80d30c79c8b6cf6c5
rupyb <rupybrisman@yahoo.com>
2018-06-12 18:54:02 -0400

    js

---
+++ 52/script.js
@@ -0,0 +1,8 @@
+console.log(name);
+console.log(email);
+
+var userInput = window.prompt("enter name");
+console.log(userInput);
+var userAge = window.prompt("enter age");
+console.log(userAge);
+alert("Your name is "+userInput+" you are "+userAge+" years old");

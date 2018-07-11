fa94e5472adfa35f860fdde654cf639b29b014fa
rupyb <rupybrisman@yahoo.com>
2018-07-11 12:32:36 -0400

    new

--- 57/script.js
+++ 57/script.js
@@ -1,8 +1,12 @@
 (function () {
     const theButton = document.getElementById('theButton');
     const theDiv = document.getElementById('theDiv');
-    theButton.addEventListener('click', () => {
+    const theBody = document.getElementById('theBody');
+    theButton.addEventListener('click', (event) => {
         const fontColor = document.getElementById('fontColor').value;
         theDiv.style.backgroundColor = fontColor;
+        theButton.style.color = fontColor;
+        theBody.style.color = 'pink';
+        console.log(event);
     });
 }());

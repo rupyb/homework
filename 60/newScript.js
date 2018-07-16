ef72b005c6c86a05df288ef6f1eadd2413ad1ba4
rupyb <rupybrisman@yahoo.com>
2018-07-16 16:19:42 -0400

    new

---
+++ 60/newScript.js
@@ -0,0 +1,81 @@
+var clockApp = clockApp || {};
+
+clockApp.getClock = (function () {
+    'use strict';
+
+
+    function newClock() {
+        let hour;
+        let minute;
+        let second;
+        let clockDiv;
+        let clock;
+        let time;
+        clockDiv = document.createElement('div');
+        clock = document.createElement('p');
+        clockDiv.appendChild(clock);
+        document.body.appendChild(clockDiv);
+        styleClock(clockDiv);
+        styleClockNumbers(clock);
+
+        time = '12:00:00';
+        clock.innerHTML = time;
+        hour = 12;
+        minute = 0;
+        second = 0;
+        setInterval(() => {
+            if (second === 59) {
+                second = 0;
+                if (minute === 59) {
+                    minute = 0;
+                    if (hour === 12) {
+                        hour = 1;
+                    } else {
+                        hour++;
+                    }
+                } else {
+                    minute++;
+                }
+            } else {
+                second++;
+            }
+            hour = formatTime(hour);
+            minute = formatTime(minute);
+            second = formatTime(second);
+            time = `${hour}:${minute}:${second}`;
+            clock.innerHTML = time;
+        }, 1000);
+
+
+        function formatTime(num) {
+            const string = num.toString();
+            if (string.length < 2) {
+                num = (num < 10 ? `0${num}` : num);
+            }
+            return num;
+        }
+
+        function styleClock(elem) {
+            elem.style.height = '70px';
+            elem.style.width = '105px';
+            elem.style.display = 'inline-block';
+            elem.style.textAlign = 'center';
+            elem.style.backgroundColor = 'aliceblue';
+        }
+
+        function styleClockNumbers(elem) {
+            elem.style.fontSize = '24px';
+            elem.style.color = 'goldenrod';
+        }
+    }
+    return {
+        newClock,
+    };
+}());
+console.log(clockApp.getClock);
+const a = clockApp.getClock;
+const b = clockApp.getClock;
+console.log('a', a);
+console.log('b', b);
+b.newClock();
+a.newClock();

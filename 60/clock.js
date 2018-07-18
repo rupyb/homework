a9518258b15620c13a14c6fb1e3bfeb47f7f0bce
rupyb <rupybrisman@yahoo.com>
2018-07-18 11:12:07 -0400

    fixed bugs

--- 60/clock.js
+++ 60/clock.js
@@ -11,6 +11,7 @@ obj.clockFace = (function (param) {
     const hour = document.createElementNS('http://www.w3.org/2000/svg', 'line');
     const minute = document.createElementNS('http://www.w3.org/2000/svg', 'line');
     const second = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
 
     svg.appendChild(circle);
     document.body.appendChild(svg);
@@ -21,35 +22,77 @@ obj.clockFace = (function (param) {
     svg.style.textAlign = 'center';
     const realTime = param;
 
+    text.setAttribute('x', '55');
+    text.setAttribute('y', '85');
+    text.setAttribute('fill', 'red');
+    text.style.fontSize = '20px';
+    text.innerHTML = 'Pcs';
+
+    circle.setAttribute('cx', '65');
+    circle.setAttribute('cy', '65');
+    circle.setAttribute('r', '56');
+    circle.setAttribute('stroke', 'green');
+    circle.setAttribute('stroke-width', '4');
+    circle.setAttribute('fill', 'white');
+
+    midCircle.setAttribute('cx', '65');
+    midCircle.setAttribute('cy', '65');
+    midCircle.setAttribute('r', '4');
+    midCircle.setAttribute('stroke-width', '4');
+    midCircle.setAttribute('fill', 'black');
+
+    const y1 = 65;
+    const radius = 56;
+    let y2 = y1 - radius;
+
+    second.setAttribute('x1', '65');
+    second.setAttribute('y1', '65');
+    second.setAttribute('x2', '65');
+    second.setAttribute('y2', y2);
+    second.style.stroke = 'black';
+    second.style.strokeWidth = '2px';
+    second.style.transformOrigin = '65px 65px';
+    second.style.transition = 'transform .5s ease-in-out';
+
+    y2 += 7;
+    minute.setAttribute('x1', '65');
+    minute.setAttribute('y1', '65');
+    minute.setAttribute('x2', '65');
+    minute.setAttribute('y2', y2);
+    minute.style.stroke = 'black';
+    minute.style.strokeWidth = '3px';
+    minute.style.transformOrigin = '65px 65px';
+    // minute.style.transition = 'transform .5s ease-in-out';
+
+    y2 += 10;
+
+    hour.setAttribute('x1', '65');
+    hour.setAttribute('y1', '65');
+    hour.setAttribute('x2', '65');
+    hour.setAttribute('y2', y2);
+    hour.style.stroke = 'black';
+    hour.style.strokeWidth = '4px';
+    hour.style.transformOrigin = '65px 65px';
+    hour.style.transition = 'transform .5s ease-in-out';
+
+    svg.appendChild(hour);
+    svg.appendChild(minute);
+    svg.appendChild(second);
+    svg.appendChild(text);
+    let configSeconds = 0;
+    start();
+
     setInterval(start, 1000);
     function start() {
         const date = new Date();
-        const realSeconds = date.getSeconds();
+        let realSeconds = date.getSeconds();
+        if (realSeconds === 0) {
+            configSeconds += 60;
+        }
+        realSeconds += configSeconds;
+        console.log(realSeconds);
         const realMinutes = date.getMinutes();
         const realHours = date.getHours();
-
-        svg.appendChild(hour);
-        svg.appendChild(minute);
-        svg.appendChild(second);
-
-        circle.setAttribute('cx', '65');
-        circle.setAttribute('cy', '65');
-        circle.setAttribute('r', '55');
-        circle.setAttribute('stroke', 'green');
-        circle.setAttribute('stroke-width', '4');
-        circle.setAttribute('fill', 'white');
-
-        midCircle.setAttribute('cx', '65');
-        midCircle.setAttribute('cy', '65');
-        midCircle.setAttribute('r', '4');
-        midCircle.setAttribute('stroke-width', '4');
-        midCircle.setAttribute('fill', 'black');
-
-        const radius = 55;
-        const x1 = 65;
-        const y1 = 65;
-        const x2 = 65;
-        let y2 = y1 - radius;
 
         if (realTime) {
             posSecond = realSeconds * 6;
@@ -58,61 +101,35 @@ obj.clockFace = (function (param) {
         }
 
         let degreeString = `rotate(${posSecond}deg)`;
-
-        second.setAttribute('x1', x1);
-        second.setAttribute('y1', y1);
-        second.setAttribute('x2', x2);
-        second.setAttribute('y2', y2);
-        second.style.stroke = 'black';
-        second.style.strokeWidth = '2px';
-        second.style.transformOrigin = '65px 65px';
-        second.style.transition = 'transform .5s ease-in-out';
         second.style.transform = degreeString;
 
         if (realTime) {
             posMinute = realMinutes * 6;
         } else if (posSecond === 360) {
-            posSecond = 0;
+            //  posSecond = 0;
             posMinute += 6;
         }
 
         degreeString = `rotate(${posMinute}deg)`;
-        y2 += 7;
-        minute.setAttribute('x1', x1);
-        minute.setAttribute('y1', y1);
-        minute.setAttribute('x2', x2);
-        minute.setAttribute('y2', y2);
-        minute.style.stroke = 'black';
-        minute.style.strokeWidth = '3px';
-        minute.style.transformOrigin = '65px 65px';
-        minute.style.transition = 'transform .5s ease-in-out';
+
         minute.style.transform = degreeString;
 
         if (realTime) {
             posHour = realHours * 30;
         } else {
             if (posMinute === 360) {
-                posMinute = 0;
+                // posMinute = 0;
             }
             if (posMinute > 90) {
                 posHour += posMinute / 45;
             }
             if (posHour === 360) {
-                posHour = 0;
+                // posHour = 0;
             }
         }
 
         degreeString = `rotate(${posHour}deg)`;
-        y2 += 10;
 
-        hour.setAttribute('x1', x1);
-        hour.setAttribute('y1', y1);
-        hour.setAttribute('x2', x2);
-        hour.setAttribute('y2', y2);
-        hour.style.stroke = 'black';
-        hour.style.strokeWidth = '4px';
-        hour.style.transformOrigin = '65px 65px';
-        hour.style.transition = 'transform .5s ease-in-out';
         hour.style.transform = degreeString;
     }
 });

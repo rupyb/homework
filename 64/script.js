70acc744199dca397a5bb11f864b8689f61fbf9f
rupyb <rupybrisman@yahoo.com>
2018-08-22 10:50:39 -0400

    new

--- 64/script.js
+++ 64/script.js
@@ -1,25 +1,23 @@
-(function() {
-'use strict';
+(function () {
+    'use strict';
 
-const vidDiv = $('#vidDiv');
-const select = $('select');
-$.getJSON('vids.json', (loadedData) => {
-    loadedData.forEach(element => {
-        const newOption = $(`<option value="${element.name}" data_url="${element.url}" data_pic="${element.picture}">${element.name}</option>`);
-        select.append(newOption);
-    });
-    select.on('change', () => {
-        vidDiv.empty();
-        const input = $('select option:selected');
-        input.each(() => {
-            console.log();
-            const newDiv = $(`<div class="${input[0].attributes.value.value}"><p>${input[0].attributes.value.value}</p></div>`);
-            const vid = $(`<video src="${input[0].attributes.data_url.value}" width="640" height="480" poster="${input[0].attributes.data_pic.value}" controls></video>`);
-            vidDiv.append(newDiv);
-            newDiv.append(vid);
+    const vidDiv = $('#vidDiv');
+    const select = $('select');
+    $.getJSON('vids.json', (loadedData) => {
+        loadedData.forEach(element => {
+            const newOption = $(`<option value="${element.name}" data_url="${element.url}" data_pic="${element.picture}">${element.name}</option>`);
+            select.append(newOption);
         });
-
+        select.on('change', () => {
+            vidDiv.empty();
+            const input = $('select option:selected');
+            input.each(() => {
+                console.log();
+                const newDiv = $(`<div class="${input[0].attributes.value.value}"><p>${input[0].attributes.value.value}</p></div>`);
+                const vid = $(`<video src="${input[0].attributes.data_url.value}" width="640" height="480" poster="${input[0].attributes.data_pic.value}" controls></video>`);
+                vidDiv.append(newDiv);
+                newDiv.append(vid);
+            });
+        });
     });
-
-});
-})();
+}());
